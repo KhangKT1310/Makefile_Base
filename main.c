cf56e666db7e6d93173a4f43edb3fa6d88d7c047
TrongKhang1310 <khangkieutrong@gmail.com>
2022-08-22 13:21:30 +0700

    update author

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/*
+ * File Name: main.c
+ * Project: Linux Programing
+ * File Created: 22-08-2022  12:40:38:PM
+ * Author: KhangKT
+ * AuthorEmail: khangkt@fpt.com.vn
+ */
+
 #include <stdio.h>
 
 
