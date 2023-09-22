20ca5f1cc81890a7e0291d84b7174fc8d7dbe813
khangkt <khangkieutrong@gmail.com>
2023-09-22 10:33:02 +0700

    upload base script build project

---
+++ baseSource.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Create a base project tree 
+mkdir -p $1/source
+mkdir -p $1/include
+mkdir -p $1/libs
+mkdir -p $1/doc
+mkdir -p $1/dependence
+
+curl https://raw.githubusercontent.com/KhangKT1310/Makefile_Base/develop/source/main.c > $1/source/main.c
+curl https://raw.githubusercontent.com/KhangKT1310/Makefile_Base/develop/source/Makefile.mk > $1/source/Makefile.mk
+curl https://raw.githubusercontent.com/KhangKT1310/Makefile_Base/develop/Makefile > $1/Makefile
+
